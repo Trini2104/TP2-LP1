e497f6bfbda8de0acb902721dcc287b3053e1846
Cami Ramat <camilaramat@gmail.com>
2022-04-19 19:43:32 -0300

    .

--- TP2-LP1/cPasajero.cpp
+++ TP2-LP1/cPasajero.cpp
@@ -19,25 +19,4 @@ bool AgregarEquipaje() {
 	}
 else
 return true;
-};#include<iostream>
-#include<string>
-#include"cPasajero.h"
-
-using namespace std;
-
-cPasajero::cPasajero()
-{
-	
-}
-cPasajero::~cPasajero() {};
-bool AgregarEquipaje() {
-	if (cantequipaje != 0)
-	{
-		if (< 25)
-			return false;
-		else
-			return true;
-	}
-	else
-return true;
 };
