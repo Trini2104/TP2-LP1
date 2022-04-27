cf0b6489f41eff51d3a00b8a046a7d29022726da
camilaliaudat <camilaliaudat@gmail.com>
2022-04-27 20:08:24 -0300

    acomodamos la funcion de asignarFechaYhora, volvimos a chequear la funcion porcentajedevuelosenhorario (funciona)

--- TP2-LP1/cFecha.cpp
+++ TP2-LP1/cFecha.cpp
@@ -5,6 +5,18 @@
 using namespace std;
 
 
-cFecha::cFecha(int _diaDespegue, int _diaLlegada, int _horaDespegue, int _horaLlegada, int _mesDespegue, int _mesLlegada, int _anioDespegue, int _anioLlegada, estadoHorario _horario) {};
+cFecha::cFecha(int _diaDespegue, int _diaLlegada, int _horaDespegue, int _horaLlegada, int _mesDespegue, int _mesLlegada, int _anioDespegue, int _anioLlegada, estadoHorario _horario) 
+{
+	this->anioDespegue = _anioDespegue;
+	this->anioLlegada = _anioLlegada;
+	this->diaDespegue = _diaDespegue;
+	this->diaLlegada = _diaLlegada;
+	this->estado = _horario;
+	this->horaDespegue = _horaDespegue;
+	this->horaLlegada = _horaLlegada;
+	this->mesDespegue = _mesDespegue;
+	this->mesLlegada = _mesLlegada;
+
+};
 cFecha::~cFecha() {};
 
--- TP2-LP1/cFecha.h
+++ TP2-LP1/cFecha.h
@@ -11,7 +11,7 @@ public:
 	int mesDespegue, mesLlegada;
 	int anioDespegue, anioLlegada;
 	estadoHorario estado;//demora, e horario
-	
+	friend class cListaVuelos;
 	cFecha(int _diaDespegue, int _diaLlegada, int _horaDespegue, int _horaLlegada, int _mesDespegue, int _mesLlegada, int _anioDespegue, int _anioLlegada, estadoHorario _horario);
 	~cFecha();
 	void setestadohorario(estadoHorario(estado))
